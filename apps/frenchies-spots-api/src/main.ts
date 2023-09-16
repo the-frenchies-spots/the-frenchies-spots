fb98f2653d6a185c26bc7c06093be55d17e2f7e0
JF-PS <jfps.dev21@gmail.com>
2023-09-16 20:38:52 +0200

    fix(frenchies-spot): cors

--- apps/frenchies-spots-api/src/main.ts
+++ apps/frenchies-spots-api/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: [process.env.FRONT_PROD_URL, process.env.FRONT_DEV_URL],
+    origin: [
+      process.env.FRONT_PROD_URL,
+      process.env.FRONT_DEV_URL,
+      process.env.FRONT_PROD_URL + '/',
+      process.env.FRONT_DEV_URL + '/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
