39d0cbfa1b746936d8405ec9fe4f2ee2085c1e65
MorganeDuluc <morgane.duluc@gmail.com>
2023-09-16 15:45:29 +0200

    feat(the-frenchies-spots-api): test of test wip

--- apps/frenchies-spots-api/test/test.spec.ts
+++ apps/frenchies-spots-api/test/test.spec.ts
@@ -2,23 +2,102 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/service/prisma.service';
+import { SpotBusiness } from '../src/business/spot.business';
+import { SpotsInput } from '../src/dto/input/spot/spots-input';
+import { Prisma } from '@prisma/client';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+  let spotBusiness: SpotBusiness;
+
+  const mockPrismaService = {
+    post: {
+      getAll: jest.fn().mockResolvedValue([
+        {
+          id: "0",
+          name: "Tour Eiffel",
+          description: "Description Tour Eiffel",
+          address: "Paris, France",
+          isCanPark: true, 
+          isHidden: false,
+          category: "SPARE_TIME_SPOT",
+        },
+        {
+          id: "1",
+          name: "Mont Saint Michel",
+          description: "Description Mont Saint Michel",
+          address: "Normandie, France",
+          isCanPark: true, 
+          isHidden: true,
+          category: "SPARE_TIME_SPOT",
+        },
+        {
+          id: "2",
+          name: "Cité du vin",
+          description: "Description Cité du vin",
+          address: "Bordeaux, France",
+          isCanPark: true, 
+          isHidden: true,
+          category: "RESOURCES_SPOT",
+        },
+      ]),
+
+      getById: jest.fn().mockResolvedValue({
+        id: "0",
+        name: "Tour Eiffel",
+        description: "Description Tour Eiffel",
+        address: "Paris, France",
+        isCanPark: true, 
+        isHidden: false,
+        category: "SPARE_TIME_SPOT",
+      }),
+
+      update: jest.fn().mockResolvedValue({}),
+      create: jest.fn().mockResolvedValue({}),
+      delete: jest.fn().mockResolvedValue({}),
+    },
+  };
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).overrideProvider(PrismaService).useValue(mockPrismaService).compile();
 
-    app = moduleFixture.createNestApplication();
+    // const module: TestingModule = await Test.createTestingModule({
+    //   imports: [AppModule],
+    // }).compile();
+
+    spotBusiness = module.get<SpotBusiness>(SpotBusiness);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    app = module.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should get one spot by id', async () => {
+    // expect(await spotBusiness.getById("0")).toBe({
+    //   id: "0",
+    //   name: "Tour Eiffel",
+    //   description: "Description Tour Eiffel",
+    //   address: "Paris, France",
+    //   isCanPark: true, 
+    //   isHidden: false,
+    //   category: "SPARE_TIME_SPOT",
+    // });
+
     return request(app.getHttpServer())
-      .get('/')
+      .post('/slot/0')
       .expect(200)
-      .expect('Hello World!');
+      .expect(spotBusiness.getById("0"));
+
+    //situation 1 : tableau vide car pas de spot sur les rayons
+    //situation 2 : tableau avec spots
+    //situation 3 : error
+  });
+
+  afterAll(async () => {
+    app.close();
   });
 });
