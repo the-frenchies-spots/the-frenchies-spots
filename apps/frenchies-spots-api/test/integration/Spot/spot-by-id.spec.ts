720fde8c1e806abdbc64a444050ea463e0408cfd
JF-PS <jfps.dev21@gmail.com>
2023-09-25 12:17:20 +0200

    feat(frenchies-spots-api): config jwt test

--- apps/frenchies-spots-api/test/integration/Spot/spot-by-id.spec.ts
+++ apps/frenchies-spots-api/test/integration/Spot/spot-by-id.spec.ts
@@ -1,1 +1,73 @@
-// describe('getSpotById', () => {});
+import { Test, TestingModule } from '@nestjs/testing';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+import { SpotRepository } from '../../../src/repository/spot.repository';
+import { queries } from '@frenchies-spots/gql';
+import { DocumentNode } from 'graphql';
+import { AuthRepository } from '../../../src/repository/auth.repository';
+import { mockAuthRepository, mockUser } from '../../mocks/mock.auth.repository';
+import { mockSpotRepository } from '../../mocks/mock.spot.repository';
+import { PublicTokenGuard } from '../../../src/guard/publicToken.guard';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { RefreshTokenGuard } from '../../../src/guard/refreshToken.guard';
+import { mockGeospatialService } from '../../mocks/mock.geospatial.service';
+import { GeospatialService } from '../../../src/service/spot-geospatial.service';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let query: (query: DocumentNode, variables?: unknown) => request.Test;
+
+  beforeEach(async () => {
+    jest.setTimeout(60000);
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideGuard(PublicTokenGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const ctx = GqlExecutionContext.create(context);
+          ctx.getContext().req.user = mockUser.user;
+          return true;
+        },
+      })
+      .overrideGuard(RefreshTokenGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const ctx = GqlExecutionContext.create(context);
+          ctx.getContext().req.user = mockUser.user;
+          return true;
+        },
+      })
+      .overrideProvider(GeospatialService)
+      .useValue(mockGeospatialService)
+      .overrideProvider(SpotRepository)
+      .useValue(mockSpotRepository)
+      .overrideProvider(AuthRepository)
+      .useValue(mockAuthRepository)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+
+    query = (query: DocumentNode, variables?: unknown) => {
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: query.loc && query.loc.source.body,
+          variables,
+        })
+        .set('authorization', `Bearer ${mockUser.refreshToken}`);
+    };
+  });
+
+  it('should get one spot by id', async () => {
+    await query(queries.spotByPk, {
+      id: 'd9b75a45-afa0-4210-8baf-49fadb8f7495',
+    }).expect(200);
+  });
+
+  afterAll(async () => {
+    app.close();
+  }, 5000);
+});
