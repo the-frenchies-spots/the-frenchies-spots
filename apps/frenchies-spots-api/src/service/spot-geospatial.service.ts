e29589b054cf2af22ed2157750176a6d3ca36ff5
JF-PS <jfps.dev21@gmail.com>
2023-09-25 16:57:34 +0200

    feat(frenchies-spots-api): test

--- apps/frenchies-spots-api/src/service/spot-geospatial.service.ts
+++ apps/frenchies-spots-api/src/service/spot-geospatial.service.ts
@@ -1,40 +1,42 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { GeoPointInput } from '../dto/input/geo-point/geo-point-input';
-import { transformDocument } from '../utils/transform-document';
-import { ProfileModel, SpotModel } from './mongoose.service';
+// import { transformDocument } from '../utils/transform-document';
+// import { ProfileModel, SpotModel } from './mongoose.service';
 
 @Injectable()
 export class GeospatialService {
   async searchPeopleArround(point: GeoPointInput): Promise<string[]> {
     const { coordinates, maxDistance } = point;
-    const profiles = await ProfileModel.find<{ _id: mongoose.Types.ObjectId }>({
-      location: {
-        $near: {
-          $geometry: {
-            type: 'Point',
-            coordinates,
-          },
-          $maxDistance: maxDistance,
-        },
-      },
-    });
-    return transformDocument(profiles);
+    // const profiles = await ProfileModel.find<{ _id: mongoose.Types.ObjectId }>({
+    //   location: {
+    //     $near: {
+    //       $geometry: {
+    //         type: 'Point',
+    //         coordinates,
+    //       },
+    //       $maxDistance: maxDistance,
+    //     },
+    //   },
+    // });
+    // return transformDocument(profiles);
+    return [];
   }
 
   async searchArround(point: GeoPointInput): Promise<string[]> {
     const { coordinates, maxDistance } = point;
-    const profiles = await SpotModel.find<{ _id: mongoose.Types.ObjectId }>({
-      location: {
-        $near: {
-          $geometry: {
-            type: 'Point',
-            coordinates,
-          },
-          $maxDistance: maxDistance,
-        },
-      },
-    });
-    return transformDocument(profiles);
+    // const profiles = await SpotModel.find<{ _id: mongoose.Types.ObjectId }>({
+    //   location: {
+    //     $near: {
+    //       $geometry: {
+    //         type: 'Point',
+    //         coordinates,
+    //       },
+    //       $maxDistance: maxDistance,
+    //     },
+    //   },
+    // });
+    // return transformDocument(profiles);
+    return [];
   }
 }
