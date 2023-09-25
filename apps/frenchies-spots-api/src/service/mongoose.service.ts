2d2b1802f11729cab57d11de7a222e2cb9533f3e
JF-PS <jfps.dev21@gmail.com>
2023-09-25 17:00:53 +0200

    feat(frenchies-spots-api): test

--- apps/frenchies-spots-api/src/service/mongoose.service.ts
+++ apps/frenchies-spots-api/src/service/mongoose.service.ts
@@ -1,27 +1,27 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+// import mongoose, { ConnectOptions } from 'mongoose';
 
-mongoose.set('strictQuery', true);
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions)
-  .then(() => {
-    // console.log('Connexion à la base de données MongoDB établie avec succès');
-  })
-  .catch((err) => {
-    console.error('Erreur lors de la connexion à la base de données :', err);
-  });
-const ProfileSchema = new mongoose.Schema({
-  pseudo: { type: String, required: true },
-});
-ProfileSchema.index({ location: '2dsphere' });
+// mongoose.set('strictQuery', true);
+// mongoose
+//   .connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   } as ConnectOptions)
+//   .then(() => {
+//     // console.log('Connexion à la base de données MongoDB établie avec succès');
+//   })
+//   .catch((err) => {
+//     console.error('Erreur lors de la connexion à la base de données :', err);
+//   });
+// const ProfileSchema = new mongoose.Schema({
+//   pseudo: { type: String, required: true },
+// });
+// ProfileSchema.index({ location: '2dsphere' });
 
-export const ProfileModel = mongoose.model('Profile', ProfileSchema, 'Profile');
+// export const ProfileModel = mongoose.model('Profile', ProfileSchema, 'Profile');
 
-const SpotSchema = new mongoose.Schema({
-  pseudo: { type: String, required: true },
-});
+// const SpotSchema = new mongoose.Schema({
+//   pseudo: { type: String, required: true },
+// });
 
-SpotSchema.index({ location: '2dsphere' });
-export const SpotModel = mongoose.model('Spot', SpotSchema, 'Spot');
+// SpotSchema.index({ location: '2dsphere' });
+// export const SpotModel = mongoose.model('Spot', SpotSchema, 'Spot');
