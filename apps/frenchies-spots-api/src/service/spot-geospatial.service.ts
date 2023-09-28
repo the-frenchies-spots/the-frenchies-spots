835afe7812253e3be35eeb635b2c84da4ebab7d1
JF-PS <jfps.dev21@gmail.com>
2023-09-28 09:17:43 +0200

    feat(frenchies-spots-api): test ci cd

--- apps/frenchies-spots-api/src/service/spot-geospatial.service.ts
+++ apps/frenchies-spots-api/src/service/spot-geospatial.service.ts
@@ -8,42 +8,40 @@ import { ProfileEntity } from '../entity/profile.entity';
 
 @Injectable()
 export class GeospatialService {
-  // constructor(
-  //   @InjectModel('Spot') private spotModel: mongoose.Model<Spot>,
-  //   @InjectModel('Profile') private profileModel: mongoose.Model<ProfileEntity>,
-  // ) {}
+  constructor(
+    @InjectModel('Spot') private spotModel: mongoose.Model<Spot>,
+    @InjectModel('Profile') private profileModel: mongoose.Model<ProfileEntity>,
+  ) {}
 
   async searchPeopleArround(
     point: GeoPointInput,
   ): Promise<{ _doc: Omit<ProfileEntity, 'id'> & { _id: string } }[]> {
     const { coordinates, maxDistance } = point;
-    // return this.profileModel.find({
-    //   location: {
-    //     $near: {
-    //       $geometry: {
-    //         type: 'Point',
-    //         coordinates,
-    //       },
-    //       $maxDistance: maxDistance,
-    //     },
-    //   },
-    // });
-    return [];
+    return this.profileModel.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates,
+          },
+          $maxDistance: maxDistance,
+        },
+      },
+    });
   }
 
   async searchArround(point: GeoPointInput): Promise<{ _id: string }[]> {
     const { coordinates, maxDistance } = point;
-    // return this.spotModel.find({
-    //   location: {
-    //     $near: {
-    //       $geometry: {
-    //         type: 'Point',
-    //         coordinates,
-    //       },
-    //       $maxDistance: maxDistance,
-    //     },
-    //   },
-    // });
-    return [];
+    return this.spotModel.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates,
+          },
+          $maxDistance: maxDistance,
+        },
+      },
+    });
   }
 }
